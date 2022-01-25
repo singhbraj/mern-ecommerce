aac1469cdc20f04af59ae103a4e826c7151784cf
singhbraj <braj@imoneypay.in>
2022-01-25 11:13:30 +0530

    some more components are added

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
-import Home from './pages/Home'
+
+  
+import Product from "./pages/Product";
+import Home from "./pages/Home";
+import ProductList from "./pages/ProductList";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import Cart from "./pages/Cart";
 
 const App = () => {
-  return <Home />
+  return <Home/>;
 };
 
 export default App;
--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -2,7 +2,7 @@ import { ArrowLeftOutlined, ArrowRightOutlined } from "@material-ui/icons";
 import { useState } from "react";
 import styled from "styled-components";
 import { sliderItems } from "../data";
-
+import { mobile } from "../responsive";
 
 const Container = styled.div`
   width: 100%;
@@ -10,7 +10,7 @@ const Container = styled.div`
   display: flex;
   position: relative;
   overflow: hidden;
- 
+  ${mobile({ display: "none" })}
 `;
 
 const Arrow = styled.div`
@@ -69,7 +69,7 @@ const Desc = styled.p`
   margin: 50px 0px;
   font-size: 20px;
   font-weight: 500;
-  letter-spacing: 2px;
+  letter-spacing: 3px;
 `;
 
 const Button = styled.button`
